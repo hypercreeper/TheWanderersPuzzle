28fcb38b85e058041e0e551d31ef037b047a7df6
Mohamad Moukayed <hyper1creeper@gmail.com>
2024-11-16 15:17:48 +0400

    clay pot

--- scripts/pot.gd
+++ scripts/pot.gd
@@ -3,7 +3,7 @@ extends Area2D
 @onready var game_manager: Node = %"Game Manager"
 @onready var coin: Area2D = $"../coin7"
 func _on_body_entered(body: Node2D) -> void:
-	self.hide()
+	coin.position = Vector2(24.0, -8.0)
 	coin.show()
-	
+	$AnimatedSprite2D.frame = 1
 	#queue_free()
