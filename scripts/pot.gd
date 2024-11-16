d2268a225773d648f2f2d3cdb1e9e02703a7adbd
Mohamad Moukayed <hyper1creeper@gmail.com>
2024-11-16 15:20:52 +0400

    fixed glitch

--- scripts/pot.gd
+++ scripts/pot.gd
@@ -1,9 +1,11 @@
 extends Area2D
-
+var broken = false
 @onready var game_manager: Node = %"Game Manager"
 @onready var coin: Area2D = $"../coin7"
+
 func _on_body_entered(body: Node2D) -> void:
-	coin.position = Vector2(24.0, -8.0)
-	coin.show()
-	$AnimatedSprite2D.frame = 1
-	#queue_free()
+	if not self.broken:
+		coin.position = Vector2(24.0, -8.0)
+		coin.show()
+		$AnimatedSprite2D.frame = 1
+		self.broken = true
