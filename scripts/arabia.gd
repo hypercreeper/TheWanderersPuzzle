f108c2c1a816c0474d6bfa8f73d78ec76c346e89
SaryNassif <sary.nassif@outlook.com>
2024-11-16 16:09:10 +0400

    water

--- scripts/arabia.gd
+++ scripts/arabia.gd
@@ -1,9 +1,24 @@
 extends Node2D
 
+@onready var water_label: Label = $Player/Camera2D/WaterLabel
+@onready var timer: Timer = $Timer
+
+
 func _ready() -> void:
 	GameManagerss.last_scene="arabia"
 	$Player/Camera2D/HBoxContainer/Label.text = str(GameManagerss.score)
-
+	timer.timeout.connect(remove_water)
+	update_water_label()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func remove_water():
+	if GameManagerss.water > 0:
+		GameManagerss.water -= 1
+		update_water_label()
+	else:
+		print("No water left!")
+
+func update_water_label():
+	print(GameManagerss.water)
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -4,6 +4,8 @@ extends Node
 static var score = 0
 var last_scene: String = ""
 
+static var water:int =15
+
 func add_point(points):
 	score +=points
 	label.text = str(score)
