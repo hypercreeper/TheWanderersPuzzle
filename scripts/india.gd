7010ecaa72e5a04608d1398adb2386e0a8977042
SaryNassif <sary.nassif@outlook.com>
2024-11-17 06:52:36 +0400

    map changes

--- scripts/india.gd
+++ scripts/india.gd
@@ -12,18 +12,6 @@ func _ready() -> void:
 	timer.timeout.connect(remove_water)
 	game_manager.update_water_label()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if regenerating_water and GameManagerss.water < 15:
-		timer.wait_time = 2
-		replenishing_label.show()
-	elif regenerating_water and GameManagerss.water >= 15:
-		timer.wait_time = 6
-		replenishing_label.hide()
-	else:
-		timer.wait_time = 6
-		replenishing_label.hide()
-
 func remove_water():
 	print(GameManagerss.water)
 	print(regenerating_water)
--- scripts/optionsbackbutton.gd
+++ scripts/optionsbackbutton.gd
@@ -2,6 +2,8 @@ extends Button
 
 @onready var button: Button = $"."
 
+var original_scale: Vector2 = Vector2.ONE
+
 func _on_button_down() -> void:
 	var tree = get_tree()
 	tree.change_scene_to_file("res://scenes/main_menu.tscn")
