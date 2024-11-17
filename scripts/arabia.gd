15c85f99e75f96471335d1b9a86306d0708f85d5
Mohamad Moukayed <hyper1creeper@gmail.com>
2024-11-17 11:40:20 +0400

    fix loop

--- scripts/arabia.gd
+++ scripts/arabia.gd
@@ -17,7 +17,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print("asd")
 	if regenerating_water:
 		replenishing_label.show()
 	else:
