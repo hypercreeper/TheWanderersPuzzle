2cee47d3b034411f2ccde6d49f62395e3b588131
Mohamad Moukayed <hyper1creeper@gmail.com>
2024-11-17 13:26:53 +0400

    finished sets

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @onready var label: Label = $"../Player/Camera2D/HBoxContainer/Label"
-static var coins = 0
+static var artifacts = 0
 static var score = 0
 static var in_convo = false
 var last_scene: String = ""
@@ -10,10 +10,10 @@ static var water:int =15
 
 func add_point(points):
 	score +=points
-	label.text = "Score: " + str(score) + "\nWater: " + str(water)
+	label.text = "Score: " + str(score) + "\n\nWater: " + str(water) + "\n\nArtifacts: " + str(artifacts)
 
 func update_water_label():
-	label.text = "Score: " + str(score) + "\nWater: " + str(water)
+	label.text = "Score: " + str(score) + "\n\nWater: " + str(water) + "\n\nArtifacts: " + str(artifacts)
 
 func kill_player():
 	pass
